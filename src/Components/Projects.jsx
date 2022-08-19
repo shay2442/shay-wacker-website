877e72f56c7acd5356afbec06eb6b29f95a9169a
Shay <shay2442@yahoo.com>
2022-08-19 11:31:23 -0500

    Link to Bye-theRoom project working

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,27 +1,24 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 
-import {
-  MainContainer,
-  Image,
-  MainContentContainer,
-  Description,
-  LinkContainer,
-  Links,
-  Title,
-} from "./ProjectStyles";
-function Projects({ imgUrl, title, link, github, description }) {
+function Projects({}) {
+
+  const navigate = useNavigate()
+
+
   return (
      <div>
-    <div className="projectsContainer">
+{/* <button className="button-images" onclick="myFunction()"><img src="/buytheroompic.png"/></button> */}
 
-    {/* <input type="image" src="https://buy-the-room-frontend.herokuapp.com/" /> */}
-
-    {/* <button onclick="myFunction()"><img src="y"/></button> */}
-    <button>Hello</button>
+<input onClick={() => window.location.replace('https://buy-the-room-frontend.herokuapp.com/rooms')}className="button-images" type="image" src="/buytheroompic.png" />
 
 
-    </div>
+
+
   <div className="tech-pics-container"> 
+
+
     <img className="tech-pics"
         src="/js.png"
         alt="logo"
