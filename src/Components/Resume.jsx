88dc85c92d3180b4768a1e030927414504a3bbd3
Shay <shay2442@yahoo.com>
2022-09-16 12:09:28 -0500

    Added ability to go stright to resume in navbar

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 function Resume() {
   const Resume = "/ShayWackerResume.pdf";
   return (
-    <div style={{ width: "100%", height: "100%" }}>
+    <div style={{ width: "1400px", height: "1000px"}}>
+
       <embed
         className="resume-pic"
         src="/ShayWackerResume.pdf"
         alt="logo"
-        height="500px"
-        width="400px"
+        // height="500px"
+        // width="400px"
       />
     </div>
   );
